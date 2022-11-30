96546419f909a870d09c6cbbff7402691abe11ef
Newton Ombese <newtonombese1@gmail.com>
2022-11-30 09:22:36 +0300

    adding hoobank logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import {navLinks} from "../constants"
 function Navbar() {
   return (
     <nav className='w-full flex py-6 justify-between items-center navbar'>
-        
+        <img src={logo} alt="hoobank" className='w-[124px] h-[32px]'/>
     </nav>
   )
 }
