1074961dde61cdbb665132e087818bf8d918f250
Newton Ombese <newtonombese1@gmail.com>
2022-11-30 08:09:53 +0300

    adding my style js file for simpler styling features

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
-import './App.css'
 
 function App() {
 
   return (
-    <div className="App">
- 
+    <div className="bg-primary w-full overflow-hidden">
+      <div>
+
+      </div>
     </div>
   )
 }
