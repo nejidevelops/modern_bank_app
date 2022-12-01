4bb6b61d3d76d11f0d37e1fa2b27e4f7288b19b1
Newton Ombese <newtonombese1@gmail.com>
2022-12-01 18:53:54 +0300

    getting started on my hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import styles from '../style'
+import { discount, robot } from '../assets'
 
 function Hero() {
   return (
-    <div>Hero</div>
+    <section id='home' className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+
+      </div>
+    </section>
   )
 }
 
