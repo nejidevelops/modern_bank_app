053625986e60f8683ac3c68f03d2438f70ea3eff
Newton Ombese <newtonombese1@gmail.com>
2022-12-01 19:05:50 +0300

    getting the first part almost done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,6 +6,13 @@ function Hero() {
   return (
     <section id='home' className={`flex md:flex-row flex-col ${styles.paddingY}`}>
       <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+        <div className='flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2'>
+          <img src={discount} alt="discount" className="w-[32px] h-[32px] " />
+          <span className='text-white'>20%</span>
+          Discount For {" "}
+          <span className='text-white'>1 Month</span>
+          Account
+        </div>
 
       </div>
     </section>
