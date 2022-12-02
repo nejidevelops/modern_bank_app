929e54d386935e97c275cb598093276641058f7d
Newton Ombese <newtonombese1@gmail.com>
2022-12-02 10:25:10 +0300

    absoluting get started into my hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from '../style'
 import { discount, robot } from '../assets'
+import GetStarted from './GetStarted'
 
 function Hero() {
   return (
@@ -17,11 +18,14 @@ function Hero() {
         </div>
 
         <div className='flex flex-row justify-between items-center w-full'>
-          <h1 className='flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px]'>
+          <h1 className='flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]'>
             The Next <br className='sm:block hidden' /> { " "}
             <span className='text-gradient'>Generation</span> { " "}
             Payment Method.
           </h1>
+          <div className='ss:flex hidden md:mr-4 mr-0'>
+            <GetStarted />
+          </div>
         </div>
       </div>
     </section>
