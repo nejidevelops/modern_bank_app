ec4d2997ac02555cdd99f892116339c7347ee362
Newton Ombese <newtonombese1@gmail.com>
2022-12-03 12:15:13 +0300

    the cards are gettig much better

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -4,8 +4,16 @@ import Button from './Button'
 
 const FeatureCard = ({icon, title, content, index}) => (
   <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? "mb-6" : "mb-0"} feature-card`}>
-    <div>
+    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
       <img src={icon} alt="icons" className='w-[50%] h-[50%] object-contain' />
+    </div>
+    <div className='flex-1 flex flex-col ml-3'>
+      <h4 className='font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1'>
+        {title}
+      </h4>
+      <p className='font-poppins font-normal text-dimWhite text-[16px] leading-[24px] mb-1'>
+        {content}
+      </p>
     </div>
   </div>
 )
