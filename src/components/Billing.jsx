9796733f98bb634c121fedc56eb9a3cacf8c2fa5
Newton Ombese <newtonombese1@gmail.com>
2022-12-03 12:33:58 +0300

    wow the billing section is ready

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -14,6 +14,11 @@ function Billing() {
       <div className={layout.sectionInfo}>
         <h2 className={styles.heading2}>Easily control your <br className="sm:block hidden" /> billing & invoicing.</h2>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum, numquam. Facere vel ducimus eligendi recusandae. Nostrum ullam similique architecto quis consequatur dolorem, laborum saepe, officia cumque necessitatibus quasi voluptate! Officia!</p>
+        <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+          <img src={apple} alt="app-store" className='w-[128px] h-[42px] object-contain mr-5 cursor-pointer' />
+          <img src={google} alt="google-play" className='w-[128px] h-[42px] object-contain mr-5 cursor-pointer' />
+
+        </div>
       </div>
     </section>
   )
