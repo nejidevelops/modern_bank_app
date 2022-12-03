bd59b2393072a96023859a6405dfe20dcdadf203
Newton Ombese <newtonombese1@gmail.com>
2022-12-03 11:53:29 +0300

    it looks nicer as much is done to it

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -6,7 +6,10 @@ function Business() {
   return (
     <section id='features' className={layout.section}>
       <div className={layout.sectionInfo}>
-        <h2 className={layout.heading2}>You do the business, <br className='sm:block hidden' /> we'll handle the money.</h2>
+        <h2 className={styles.heading2}>You do the business, <br className='sm:block hidden' /> we'll handle the money.</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>With the right credit card, you can improve your financial life by building credit, earning rewards and saving money. But with hundreds of credit cards on the market.</p>
+
+        <Button styles="mt-10" />
       </div>
     </section>
   )
