cfc2254904b83feea93fcc26cdab0aac9dfedcb9
Newton Ombese <newtonombese1@gmail.com>
2022-12-03 12:24:35 +0300

    working on some gradient colors on the billing section

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -3,7 +3,14 @@ import styles, { layout } from '../style'
 
 function Billing() {
   return (
-    <div></div>
+    <section id="product" className={layout.sectionReverse}>
+      <div className={layout.sectionImgReverse}>
+        <img src={bill} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+
+        <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+        <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient" />
+      </div>
+    </section>
   )
 }
 
