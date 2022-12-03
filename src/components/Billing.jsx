a2345b5e57a4af8d091228ab365ccd9040171fbe
Newton Ombese <newtonombese1@gmail.com>
2022-12-03 12:29:19 +0300

    billing section is heading to completion as it is super responsive

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -10,6 +10,11 @@ function Billing() {
         <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
         <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient" />
       </div>
+
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>Easily control your <br className="sm:block hidden" /> billing & invoicing.</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum, numquam. Facere vel ducimus eligendi recusandae. Nostrum ullam similique architecto quis consequatur dolorem, laborum saepe, officia cumque necessitatibus quasi voluptate! Officia!</p>
+      </div>
     </section>
   )
 }
