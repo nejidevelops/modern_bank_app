2c67ad081f11114c7ef1e2edf6e2265a53c2e19b
Newton Ombese <newtonombese1@gmail.com>
2022-12-03 12:04:56 +0300

    successful creation of the featurecard reusable component

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -2,6 +2,14 @@ import { features } from '../constants'
 import styles, { layout } from '../style'
 import Button from './Button'
 
+const FeatureCard = ({icon, title, content, index}) => (
+  <div>
+    <div>
+      <img src={icon} alt="icons" />
+    </div>
+  </div>
+)
+
 function Business() {
   return (
     <section id='features' className={layout.section}>
@@ -10,6 +18,12 @@ function Business() {
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>With the right credit card, you can improve your financial life by building credit, earning rewards and saving money. But with hundreds of credit cards on the market.</p>
 
         <Button styles="mt-10" />
+      </div>
+
+      <div className={`${layout.sectionImg} flex-col`}>
+        {features.map((feature, index)=>(
+          <FeatureCard key={feature.id} {...feature} index={index} />
+        ))}
       </div>
     </section>
   )
