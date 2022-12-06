03a007688828dc9c6635dc22d48830df9e50bb97
Newton Ombese <newtonombese1@gmail.com>
2022-12-06 17:57:08 +0300

    everything in the card deal component is well and ready for the website

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -10,6 +10,10 @@ function CardDeal() {
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt similique eum labore in neque tenetur facere eveniet maxime maiores rerum deserunt sed nobis incidunt, culpa natus mollitia expedita molestiae vitae.</p>
       </div>
       <Button styles="mt-10" />
+
+      <div className={layout.sectionImg}>
+        <img src={card} alt="card" className="w-[100%] h-[100%]" />
+      </div>
     </section>
   )
 }
