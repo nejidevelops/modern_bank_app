ee64882c6a92c4e248301745bb1fb4a4b832fa93
Newton Ombese <newtonombese1@gmail.com>
2022-12-06 18:19:15 +0300

    the feedback card has got only the title to be complete

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -3,7 +3,11 @@ import { quotes } from '../assets'
 function Feedback({ content, name, title, img}) {
   return (
     <div className='flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card'>
-      <img src={quotes} />
+      <img src={quotes} alt="double_quotes" className='w-[42px] h-[72px] object-contain' />
+      <p className='font-poppins font-normal text-[18px] leading-[32px] text-white my-10'>{content}</p>
+      <div>
+        <img src={img} alt={name} className="w-[48px] h-[48px] rounded-full" />
+      </div>
     </div>
   )
 }
