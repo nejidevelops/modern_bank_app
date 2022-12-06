3500a64708aad50ba429e3a1891d2a7ddbf630bd
Newton Ombese <newtonombese1@gmail.com>
2022-12-06 18:11:27 +0300

    ready to map my feedback data from constants

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -16,7 +16,7 @@ function Testimonials() {
 
       <div className='flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]'>
         {feedback.map((card)=>(
-          <
+          <FeedbackCard key={card.id} {...card} />
         ))}
       </div>
     </section>
