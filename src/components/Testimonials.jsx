a5fad6c0d2b62ac964af69eb7331e5ff81691266
Newton Ombese <newtonombese1@gmail.com>
2022-12-06 18:06:02 +0300

    testing the little piece of data and information to confirm that it is getting well as it should

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,13 +1,16 @@
 import { feedback } from '../constants'
 import styles from '../style'
-import FeedbackCard from './FeedbackCard'
+// import FeedbackCard from './FeedbackCard'
 
 function Testimonials() {
   return (
     <section id='clients' className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
       <div />
-      <div>
-        
+      <div className='w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]'>
+        <h1 className={`${styles.heading2}`}>What people are <br className='sm:block hidden' />saying about Us</h1>
+        <div className='w-full md:mt-0 mt-6'>
+          <p>Everything you need to accept card payments and grow your business anywhere on the planet</p>
+        </div>
       </div>
     </section>
   )
