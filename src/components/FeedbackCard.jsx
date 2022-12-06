5871113fd6f12c552c69bdec7bfd6fc943af4c0e
Newton Ombese <newtonombese1@gmail.com>
2022-12-06 18:15:49 +0300

    progress excites everyone

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
-
-function Feedback() {
+import { quotes } from '../assets'
+ 
+function Feedback({ content, name, title, img}) {
   return (
-    
+    <div className='flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card'>
+      <img src={quotes} />
+    </div>
   )
 }
 
