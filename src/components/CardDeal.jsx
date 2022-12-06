3091a82ea01a67ac98f73660c28f55c0d9413502
Newton Ombese <newtonombese1@gmail.com>
2022-12-06 17:52:19 +0300

    swift progress on the card deal component

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -4,8 +4,11 @@ import Button from "./Button"
 
 function CardDeal() {
   return (
-    <section>
-
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>Find a better card deal <br className="sm:block hid" />in few easy steps</h2>
+        <p>Lorem</p>
+      </div>
     </section>
   )
 }
