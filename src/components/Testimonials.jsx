66a406d5c7f6c8fa29d544816d47f3ccc5003849
Newton Ombese <newtonombese1@gmail.com>
2022-12-06 18:01:11 +0300

    getting testimonial imports correctly

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,9 +1,14 @@
-
+import { feedback } from '../constants'
+import styles from '../style'
+import FeedbackCard from './FeedbackCard'
 
 function Testimonials() {
   return (
-    <section>
-      
+    <section id='clients' className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
+      <div />
+      <div>
+        
+      </div>
     </section>
   )
 }
