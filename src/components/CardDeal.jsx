4e439bc950c334bdbd90f0e3d5b0cdbc7fd3b194
Newton Ombese <newtonombese1@gmail.com>
2022-12-06 17:55:10 +0300

    slight change on the button component by adding a border radius of 10px

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -7,8 +7,9 @@ function CardDeal() {
     <section className={layout.section}>
       <div className={layout.sectionInfo}>
         <h2 className={styles.heading2}>Find a better card deal <br className="sm:block hid" />in few easy steps</h2>
-        <p>Lorem</p>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt similique eum labore in neque tenetur facere eveniet maxime maiores rerum deserunt sed nobis incidunt, culpa natus mollitia expedita molestiae vitae.</p>
       </div>
+      <Button styles="mt-10" />
     </section>
   )
 }
