96a440bc165bec941031be085d0077f9da680994
Newton Ombese <newtonombese1@gmail.com>
2022-12-06 18:49:56 +0300

    getting my footer ready and working but it comes after a number of failed trials

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,17 @@ import { footerLinks, socialMedia } from '../constants'
 
 function Footer() {
   return (
-   
+   <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
+    <div className={`${styles.flexCenter} md:flex-row flex-col mb-8 w-full`}>
+      <div className="flex-1 flex flex-col justify-start mr-10">
+        <img src={logo} alt="hoobank" className="w-[266px] h-[72px] object-contain" />
+        <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>A new way to make the payments easy, reliable and secure.</p>
+      </div>
+      <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
+
+      </div>
+    </div>
+   </section>
   )
 }
 
